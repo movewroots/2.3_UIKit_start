0c85432d8e9b684939491838a7f312bc56b1086e
movewroots <movinonwithroots@gmail.com>
2022-12-07 21:26:30 +0300

    Initial Commit

---
+++ 2.3_UIKit_start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.3_UIKit_start
+//
+//  Created by Nikita Verkhoturov on 07.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
